f7de316ce9da22f9b9cb863c89034719a50e3f01
vdotsenko <dotsenko.vitaliy@gmail.com>
2022-07-19 12:16:46 +0300

    add endeavourOS install-sh

---
+++ arch/endeavour/fresh-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# system
+sudo pacman -S ksysguard
+
+# editors
+sudo pacman -S neovim
+sudo pacman -S geany
+yay -S sublime-text-4
+yay -S sublime-merge
+
+# www
+yay -S skypeforlinux-stable-bin
