9019292f1cdbe9e8fc80a4da29f17edc61b6d538
vdotsenko <dotsenko.vitaliy@gmail.com>
2023-03-20 17:34:28 +0200

    improvements

--- arch/endeavour/fresh-install.sh
+++ arch/endeavour/fresh-install.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 
-## system
-sudo pacman -S ksysguard
+## sys
+sudo pacman -S ksysguard filelight
+sudo pacman -S zip unzip
 
-## editors
-sudo pacman -S neovim
-sudo pacman -S geany
+## edit
+#sudo pacman -S neovim
+#sudo pacman -S geany
 yay -S sublime-text-4
 yay -S sublime-merge
 
+sudo pacman -S code
+
+
+## develop
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl -s "https://get.sdkman.io" | bash
+
 
 ## www
-yay -S skypeforlinux-stable-bin
 # for skype
 sudo pacman -S gnome-keyring
+# skype
+yay -S skypeforlinux-stable-bin #needs reboot
+
+
+## fun
+sudo pacman -S steam
+
+### Manual Section:
+# 1 #
+# Virtual Desktops: 4
+# Screen Edges: No action
+# 2 #
+# win + t = konsole
+# win + w = firefox
+# win + f = dolphin
+# win + space = switch layout
+# 3 #
+# widgets : total cpu use, fokus
