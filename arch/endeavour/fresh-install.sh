362770fa527add24296ffd3c6c2b6713b0cbaea8
vdotsenko <dotsenko.vitaliy@gmail.com>
2024-06-06 16:22:09 +0300

    update endeavour script

--- arch/endeavour/fresh-install.sh
+++ arch/endeavour/fresh-install.sh
@@ -4,35 +4,35 @@
 sudo pacman -S filelight htop
 sudo pacman -S zip unzip krusader kdiff3 krename
 sudo pacman -S sshfs fuse freerdp
+sudo pacman -S dbeaver
 
 ## develop
 #sudo pacman -S neovim
 #sudo pacman -S geany
 yay -S sublime-text-4
 yay -S sublime-merge
+yay -S vscodium-bin
 
-sudo pacman -S code
 
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 curl -s "https://get.sdkman.io" | bash
 
-#configure ssh-agent
-echo "SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket" > ~/.bashrc
-echo "[Unit]
-Description=SSH key agent
+##configure ssh-agent
+#echo "SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket" > ~/.bashrc
+#echo "[Unit]
+#Description=SSH key agent
 
-[Service]
-Type=simple
-Environment=SSH_AUTH_SOCK=%t/ssh-agent.socket
-# DISPLAY required for ssh-askpass to work
-Environment=DISPLAY=:0
-ExecStart=/usr/bin/ssh-agent -D -a $SSH_AUTH_SOCK
+#[Service]
+#Type=simple
+#Environment=SSH_AUTH_SOCK=%t/ssh-agent.socket
+## DISPLAY required for ssh-askpass to work
+#Environment=DISPLAY=:0
+#ExecStart=/usr/bin/ssh-agent -D -a $SSH_AUTH_SOCK
 
-[Install]
-WantedBy=default.target" > ~/.config/systemd/user/ssh-agent.service
+##[Install]
+#WantedBy=default.target" > ~/.config/systemd/user/ssh-agent.service
 
-systemctl --user enable ssh-agent.service
+#systemctl --user enable ssh-agent.service
 
 ## design
 
@@ -42,9 +42,9 @@ yay -S blockbench
 
 ## www
 # for skype
-sudo pacman -S gnome-keyring
+#sudo pacman -S gnome-keyring
 # skype
-yay -S skypeforlinux-stable-bin #needs reboot
+yay -S skypeforlinux-bin #needs reboot
 
 yay -S megasync-bin
 
